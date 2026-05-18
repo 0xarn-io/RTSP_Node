5a17232b1c35b0b83df3a2fe5c74f44ed2ecd00f
Arnau <zimzf23@gmail.com>
2026-05-18 17:41:06 +0200

    data

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ START = time.monotonic()
 APP_INFO = {
     "name":            "RTSP Driver Node",
     "description":     "Restful RTSP stream driver",
-    "version":         "0.1.0",
+    "version":         "0.0.2",
     "author":          "Amontplet",
     "email":           "amontplet@warak.com",
     "company":         "Warak Group",
